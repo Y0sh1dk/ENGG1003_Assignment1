0a9950b684c259e3a6a9693f716ad7c60b4233a6
Yosiah <yosiahdekoeyer@gmail.com>
2019-04-01 20:38:39 +1100

    test

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
-/* read from and too files
- *
- *
- *
- *
- *
- *
- */
+//TODO: Add user interface that allows user to pick which cipher method to use and to input a key
+//TODO: Add math library somehow
 
+
+
+// Function Prototypes:
+int rotation_cipher_decrypter();
+int rotation_cipher_encrypter();
+int substitution_cipher_encrypter();
+int substitution_cipher_decrypter();
 
 
 
@@ -16,4 +17,45 @@
 int main() {
     printf("Hello, World!\n");
     return 0;
+}
+
+
+
+/*
+ *  Function Name: rotation_cipher_encrypter
+ *
+ *  Takes a input.txt file and outputs a encrypted.txt file
+ */
+int rotation_cipher_encrypter() {
+
+}
+
+
+/*
+ *  Function Name: rotation_cipher_decrypter
+ *
+ *  Takes a encrypted.txt file and outputs a decrypted.txt file
+ */
+int rotation_cipher_decrypter(){
+
+}
+
+/*
+ *  Function Name: substitution_cipher_encrypter
+ *
+ *  Takes a input.txt file and outputs a encrypted.txt file
+ */
+int substitution_cipher_encrypter() {
+
+}
+
+
+
+/*
+ *  Function Name: substitution_cipher_decrypter
+ *
+ *  Takes a input.txt file and outputs a encrypted.txt file
+ */
+int substitution_cipher_decrypter() {
+
 }
