ee8a25478d8688da37956bdec167fab814e852f7
Yosiah <yosiahdekoeyer@gmail.com>
2019-04-09 12:20:11 +1000

    Uses files instead of strings

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 // Function Prototypes:
 int rotation_cipher_decrypter(char *string, int string_size, int key); // (array name, string length, key)
 int rotation_cipher_encrypter(char *string, int string_size, int key); // (array name, string length, key)
+void rotation_cipher_encrypter_file(char *input_path, char *output_path, int key );
 int substitution_cipher_encrypter(char *string, int string_size, char *key);
 int substitution_cipher_decrypter(char *string, int string_size, char *key);
 void array_zeroer(int *x, int N); // Not necessary?
@@ -26,6 +27,13 @@ int main() {
     printf("c) Encryption with a substitution cipher given plain text and key: \n");
     printf("d) Decryption with a substitution cipher given cipher text and key: \n\n");
     */
+    char input_path[] = "input.txt";
+    char output_path[] = "encrypted.txt";
+    int key = 2;
+
+    rotation_cipher_encrypter_file(input_path, output_path, key);
+
+/*
     char string[] = "This IS a TEST string";
     string_uppercaser(string, sizeof(string));
     int key = 6;
@@ -34,8 +42,33 @@ int main() {
     rotation_cipher_decrypter(string, sizeof(string), key);
 
     printf("%s", string);
+ */
 }
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+void rotation_cipher_encrypter_file(char *input_path, char *output_path, int key ) {
+    FILE *input;
+    FILE *output;
+    input = fopen(input_path, "r");
+    if (input == NULL) {
+        perror("fopen()");
+        return 0;
+    }
+    output = fopen(output_path, "w");
+
+    while (!feof(input)) {
+        char c;
+        fscanf(input, "%c", &c);
+        if (c >= 'A' && c <= 'Z') {
+            c = ((c - 'A' + key) % 26 + 'A');
+        }
+        fprintf(output, "%c", c);
+    }
+}
+
+
+
 
 
 /*
